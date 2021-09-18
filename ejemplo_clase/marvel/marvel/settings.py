96867c5b86631b924b1ae434de3e5fd372c95df9
Vergara <h.vergara@accenture.com>
2021-09-18 17:57:02 -0300

    cambios

--- ejemplo_clase/marvel/marvel/settings.py
+++ ejemplo_clase/marvel/marvel/settings.py
@@ -162,6 +162,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (str(BASE_DIR.joinpath('staticfiles')),)
+WHITENOISE_MANIFEST_STRICT = False
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
